939e1aa5902b219e2359e5af4c13c424b83b3dec
Daniel Oda Barcelos <158526625+danielodab@users.noreply.github.com>
2024-10-14 21:46:34 -0300

    Update database.config.js

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -3,7 +3,6 @@ config ()
 
 module.exports = {
     dialect: process.env.DB_DIALECT,
-    dialectModule: pg,
     host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
