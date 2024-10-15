648e283cd1ea0a1350a669492f0350ec2a00e6ed
Daniel Oda Barcelos <158526625+danielodab@users.noreply.github.com>
2024-10-14 21:39:33 -0300

    Update database.config.js

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -2,22 +2,23 @@ const { config } = require ('dotenv')
 config ()
 
 module.exports = {
-    dialect: process.env.DIALECT,
-    host: process.env.HOST,
-    username: process.env.USERNAMEDB,
-    password: process.env.PASSWORDDB,
-    database: process.env.DATABASE,
+    dialect: process.env.DB_DIALECT,
+    dialectModule: pg,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     port: process.env.PORT_DB,
-     dialectOptions: {
-         ssl: { 
-             rejectUnauthorized: true,
-             require: true
-         }
-     },
-     pool: {
-         max: 5,
-         min: 0,
-         acquire: 30000,
-         idle: 10000
-     }
-};
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: true,
+            require: true
+        }
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000 // idle timeout in milliseconds
+    }
+}
